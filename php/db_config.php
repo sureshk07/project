e4138a95e0ed871ec08d94c05f853eb39250f3dc
suresh <sureshkumar199873@gmail.com>
2020-11-10 17:10:12 +0530

    add new file

---
+++ php/db_config.php
@@ -0,0 +1,10 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$db_name = "sk";
+
+$con = mysqli_connect($hostname,$username,$password,$db_name);
+
+?>
---
+++ php/navbar.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">Phpwebsite</a>
+      </div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Insert</a></li>
+        <li><a href="./fetch.php">Fetech Data</a></li>
+        <li><a href="#">Update Data</a></li>
+        <li><a href="#">Delete</a></li>
+      </ul>
+    </div>
+  </nav>
