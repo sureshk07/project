c46aa5cd6fe6ee00f8a04068ea1abd6da7b1eb85
suresh <sureshkumar199873@gmail.com>
2020-11-18 14:47:53 +0530

    adding files

--- php/edit_data.php
+++ php/edit_data.php
@@ -5,13 +5,17 @@ if(isset($_POST['submit'])){
     inserData();
 }
 
+
 function inserData(){
+    $edit_id = $_GET['id'];
     include_once('db_config.php');
     $name = $_POST['uname'];
     $mobile = $_POST['umobile'];
     $email = $_POST['uemail'];
     
-    $ins_query = mysqli_query($con, "INSERT INTO users (name, mobile, email) VALUES ('$name','$mobile','$email')");
+    $ins_query = mysqli_query($con, "UPDATE users
+    SET name = '$name', email= '$email', mobile= '$mobile'
+    WHERE id = $edit_id");
     
     if($ins_query){
         header("location: fetch.php");
--- php/fetch.php
+++ php/fetch.php
@@ -13,23 +13,23 @@
         include_once('db_config.php');
         include_once('navbar.php');
 
-        $fetch_query =  mysqli_query($con,"select  *  from users");
+        $fetch_query =  mysqli_query($con,"select * from users");
         $query_rows = mysqli_num_rows($fetch_query);
         
 
         if($query_rows > 0){
-            echo "<table class='table table-bordered'><tr><th>Name</th><th>Mobile</th><th>Email</th></tr>";
+            echo "<table class='table table-bordered'><tr><th>Name</th><th>Mobile</th><th>Email</th><th></th></tr>";
             while($row = mysqli_fetch_array($fetch_query)){ 
                 echo "<tr><td>".$row['name']."</td>";
                 echo "<td>".$row['mobile']."</td>";
-                echo "<td>".$row['email']."</td></tr>";
+                echo "<td>".$row['email']."</td>";
+                echo "<td> <a href='./delete.php?id=".$row['id']."'><button class='btn btn-danger'>Delete</button></a> <a href='./edit.php?id=".$row['id']."'> <button class='btn btn-success'>Update</button></a></td></tr>";
             }
             echo "</table>";
             
         }else{
             echo "No Data found";
         }
-
     ?>
 </body>
 </html>
